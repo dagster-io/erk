9b2ab4d24ac12d884ae43958defd2131bc57fe6a
Nick Schrock <schrockn@dagsterlabs.com>
2025-12-21 11:26:20 -0800

    update

--- packages/erk-shared/src/erk_shared/github_admin/__init__.py
+++ packages/erk-shared/src/erk_shared/github_admin/__init__.py
@@ -3,7 +3,6 @@
 This package provides the ABC and types for GitHub Actions admin operations.
 """
 
-from erk_shared.github_admin.abc import AuthStatus, GitHubAdmin
-from erk_shared.github_admin.fake import FakeGitHubAdmin
-
-__all__ = ["AuthStatus", "GitHubAdmin", "FakeGitHubAdmin"]
+from erk_shared.github_admin.abc import AuthStatus as AuthStatus
+from erk_shared.github_admin.abc import GitHubAdmin as GitHubAdmin
+from erk_shared.github_admin.fake import FakeGitHubAdmin as FakeGitHubAdmin
