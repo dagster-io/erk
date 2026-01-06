553f8c8fe1557448661ecfe2ad59af32efa8fbe4
Nick Schrock <schrockn@dagsterlabs.com>
2026-01-06 10:31:20 -0800

    WIP: Prepare for PR submission

--- src/erk/cli/commands/branch/__init__.py
+++ src/erk/cli/commands/branch/__init__.py
@@ -5,6 +5,7 @@ import click
 from erk.cli.alias import alias, register_with_aliases
 from erk.cli.commands.branch.checkout_cmd import branch_checkout
 from erk.cli.commands.branch.create_cmd import branch_create
+from erk.cli.commands.branch.delete_cmd import branch_delete
 from erk.cli.commands.branch.list_cmd import branch_list
 from erk.cli.help_formatter import ErkCommandGroup
 
@@ -18,5 +19,6 @@ def branch_group() -> None:
 
 # Register subcommands
 branch_group.add_command(branch_create)
+branch_group.add_command(branch_delete)
 register_with_aliases(branch_group, branch_checkout)
 register_with_aliases(branch_group, branch_list)
