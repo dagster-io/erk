40d4097e2fc9a79b6afdd409fd1a785644caed29
Nick Schrock <schrockn@dagsterlabs.com>
2026-01-19 05:45:35 -0500

    Add plan for issue #5226

--- src/erk/cli/commands/wt/create_cmd.py
+++ src/erk/cli/commands/wt/create_cmd.py
@@ -8,6 +8,7 @@ import click
 
 from erk.cli.activation import (
     ENABLE_ACTIVATION_SCRIPTS,
+    activation_config_activate_only,
     print_activation_instructions,
     write_worktree_activate_script,
 )
@@ -980,7 +981,7 @@ def create_wt(
                 activation_script_path,
                 source_branch=None,
                 force=False,
-                mode="activate_only",
+                config=activation_config_activate_only(),
                 copy=True,
             )
     else:
@@ -990,7 +991,7 @@ def create_wt(
                 activation_script_path,
                 source_branch=None,
                 force=False,
-                mode="activate_only",
+                config=activation_config_activate_only(),
                 copy=True,
             )
 
