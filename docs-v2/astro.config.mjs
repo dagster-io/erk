8f18bbe1bd9991d49e4422c34e5cd0b58b37a045
Nick Schrock <schrockn@dagsterlabs.com>
2026-03-05 09:22:08 -0800

    cp

--- docs-v2/astro.config.mjs
+++ docs-v2/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import starlightThemeBlack from "starlight-theme-black";
 
 export default defineConfig({
   integrations: [
     starlight({
+      plugins: [starlightThemeBlack({})],
       title: "Erk",
       description: "Plan-oriented agentic engineering",
       social: [
